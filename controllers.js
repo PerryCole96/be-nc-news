475af04d4359092e7f95392cef0aff50233c6b30
Perry Cole <perrycole1996@gmail.com>
2024-10-17 12:07:49 +0100

    amended code to meet feedback surrounding errorhandling middleware

--- controllers.js
+++ controllers.js
@@ -1,77 +1,89 @@
-const { getTopics, getArticleById, getArticles, getCommentsByArticleId, addComment, updateVotes } = require('./db/models');
+const {
+  getTopics,
+  getArticleById,
+  getArticles,
+  getCommentsByArticleId,
+  addComment,
+  updateVotes,
+} = require('./db/models');
 const fs = require('fs');
 const path = require('path');
 const endpoints = require('./endpoints.json');
 
 exports.fetchTopics = (req, res, next) => {
   const { sort_by } = req.query;
-  const validSortbys = ['slug', 'description']
+  const validSortbys = ['slug', 'description'];
 
   if (sort_by && !validSortbys.includes(sort_by)) {
-    return res.status(400).send({ message: 'Bad Request!' })
+    return res.status(400).send({ message: 'Bad Request!' }); 
   }
-  
-  const sortColumn = sort_by || 'slug'
+
+  const sortColumn = sort_by || 'slug';
   getTopics(sortColumn)
     .then((topics) => {
-      res.status(200).send({ topics })
+      res.status(200).send({ topics });
     })
-    .catch(next)
+    .catch(next);
 };
 
 exports.fetchArticle = (req, res, next) => {
   const { article_id } = req.params;
 
   if (isNaN(article_id)) {
-    return res.status(400).send({ message: 'Bad Request!' })
+    return res.status(400).send({ message: 'Bad Request!' }); 
   }
 
   getArticleById(article_id)
-    .then(article => {
+    .then((article) => {
       if (!article) {
-        return res.status(404).send({ message: 'Article not found' })
+        return res.status(404).send({ message: 'Article not found' });
       }
-      res.status(200).send(article)
+      res.status(200).send({ ...article }); 
     })
-    .catch(err => {
-      console.error(err)
-      next(err)
-    })
+    .catch(next);
 };
 
 exports.fetchArticles = (req, res, next) => {
   const { sort_by } = req.query;
-
-  const validSortColumns = ['author', 'title', 'article_id', 'topic', 'created_at', 'votes', 'article_img_url', 'comment_count'];
+  const validSortColumns = [
+    'author',
+    'title',
+    'article_id',
+    'topic',
+    'created_at',
+    'votes',
+    'article_img_url',
+    'comment_count',
+  ];
 
   if (sort_by && !validSortColumns.includes(sort_by)) {
-    return res.status(400).send({ message: 'Bad Request!' })
+    return res.status(400).send({ message: 'Bad Request!' });
   }
 
-  const sortColumn = sort_by || 'created_at'
+  const sortColumn = sort_by || 'created_at';
 
   getArticles(sortColumn)
     .then((articles) => {
-      res.status(200).send({ articles })
+      res.status(200).send({ articles });
     })
-    .catch(next)
+    .catch(next);
 };
 
 exports.fetchComments = (req, res, next) => {
   const { article_id } = req.params;
 
   if (isNaN(article_id)) {
-    return res.status(400).send({ message: 'Bad Request!' })
+    return res.status(400).send({ message: 'Bad Request!' }); 
   }
 
   getCommentsByArticleId(article_id)
-    .then(comments => {
+    .then((comments) => {
       if (comments.length === 0) {
-        return res.status(404).send({ message: 'Article has no Comments' })
+        return res.status(404).send({ message: 'Article has no comments' }); 
       }
-      res.status(200).send({ comments })
+      res.status(200).send({ comments });
     })
-    .catch(next)
+    .catch(next);
 };
 
 exports.postComment = (req, res, next) => {
@@ -81,13 +93,12 @@ exports.postComment = (req, res, next) => {
   if (!username || !body) {
     return res.status(400).send({ message: 'Bad Request! Missing required fields.' });
   }
-
   if (isNaN(article_id)) {
-    return res.status(400).send({ message: 'Unable to post! Invalid article ID.' });
+    return res.status(400).send({ message: 'Bad Request!' }); 
   }
 
   getArticleById(article_id)
-    .then(article => {
+    .then((article) => {
       if (!article) {
         return res.status(404).send({ message: 'Article not found' });
       }
@@ -96,34 +107,33 @@ exports.postComment = (req, res, next) => {
     .then((comment) => {
       res.status(201).send({ comment });
     })
-    .catch(next)
-    }
+    .catch(next);
+};
 
 exports.patchArticle = (req, res, next) => {
   const { article_id } = req.params;
   const { inc_votes } = req.body;
-    
-  if (isNaN(article_id)) {
-        return res.status(400).send({ message: 'Bad Request! Invalid article ID.' })
-      }
-    
-  if (typeof inc_votes !== 'number') {
-        return res.status(400).send({ message: 'Bad Request! inc_votes must be a number.' })
-      }
-    
-  getArticleById(article_id)
-      .then((article) => {
-        if (!article) {
-          return res.status(404).send({ message: 'Article not found' });
-          }
-          return updateVotes(article_id, inc_votes);
-        })
-      .then((updatedArticle) => {
-          res.status(200).send({ article: updatedArticle });
-        })
-      .catch(next);
-    };
 
-exports.getApi = (req, res, next) => {
-  res.status(200).send({ endpoints })
+  if (isNaN(article_id)) {
+    return res.status(400).send({ message: 'Bad Request!' }); 
+  }
+  if (typeof inc_votes !== 'number') {
+    return res.status(400).send({ message: 'Bad Request! inc_votes must be a number.' });
+  }
+
+  getArticleById(article_id)
+    .then((article) => {
+      if (!article) {
+        return res.status(404).send({ message: 'Article not found' });
+      }
+      return updateVotes(article_id, inc_votes);
+    })
+    .then((updatedArticle) => {
+      res.status(200).send({ article: updatedArticle }); 
+    })
+    .catch(next);
+};
+
+exports.getApi = (req, res) => {
+  res.status(200).send({ endpoints });
 };
